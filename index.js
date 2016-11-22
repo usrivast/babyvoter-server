ed0dce4a1e6d4aadfc88b7b51a1d679a632a86e0
Srivastava, Utkarsh <Utkarsh.Srivastava@capitalone.com>
2016-11-22 00:15:59 -0500

    deployment profile

--- index.js
+++ index.js
@@ -46,10 +46,12 @@ process.on('uncaughtException', function(err) {
 
 //facebook auth setup
 options = {
-    clientID: '609091159257057',
-    clientSecret: '2e28ec2f26a0ae6794d8a8a26dbdad61',
-    // callbackURL: 'https://arcane-peak-48225.herokuapp.com/auth/facebook/callback',
-    callbackURL: 'http://localhost:3000/auth/facebook/callback',
+    clientID: '1757028757898172',
+    clientSecret: 'da21badc2845c985e556f07e884544a9',
+    callbackURL: 'https://arcane-peak-48225.herokuapp.com/auth/facebook/callback',
+    // clientID: '609091159257057',
+    // clientSecret: '2e28ec2f26a0ae6794d8a8a26dbdad61',
+    // callbackURL: 'http://localhost:3000/auth/facebook/callback',
     profileFields: ['id', 'displayName','name', 'email', 'gender', 'photos']
 };
 
